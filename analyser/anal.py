7572d3d6f5810c3cdb0a8730c4f2dd3ee600131d
Julien Bernard <julien.bernard@external.softbankrobotics.com>
2019-05-24 17:52:32 +0200

    add bench struct

--- analyser/anal.py
+++ analyser/anal.py
@@ -1,5 +1,8 @@
-# import json
-# import statistics
+import json
+import statistics
+from functools import partial
+import operator
+
 
 dump_map = lambda func, *args : (lambda *args : None)(*map(func, *args))
 
@@ -9,85 +12,161 @@ class Node:
         self.__sub = sub
         self.__con = []
         self.__has_value = False
+        self.__sticky = False
         self.__value = None
 
-        dump_map(lambda sub : sub.subscribe(self), self.__sub)
+        dump_map(lambda sub : sub.__subscribe(self), self.__sub)
 
-    def subscribe(self, con):
-        self.__con.append(con)
-
-    @property
-    def value(self):
+    # @property
+    def get(self):
         return self.__value
 
-    @value.setter
-    def value(self, value):
-        self.__value = value
-        self.__has_value = True
-        self.notify()
+    # @value.setter
+    def set(self, value):
+        self.__set_value(value, True)
 
     def has_value(self):
         return self.__has_value
 
-    def try_update(self):
-        if(all(map(Node.has_value, self.__sub))):
-            self.value = self.__op(*map(lambda n : n.value, self.__sub))
-            self.notify()
+    def __subscribe(self, con):
+        self.__con.append(con)
 
-    def notify(self):
-        dump_map(Node.try_update, self.__con)
+    def __set_value(self, value, sticky):
+        self.__value = value
+        self.__has_value = True
+        self.__sticky = sticky
+        self.__notify()
 
-    def reset(self):
-        self.__value = None
-        self.__has_value = False
-        dump_map(Node.reset, self.__con)
+
+    def __try_update(self):
+        if( not self
+        and not self.__sticky
+        and all(map(Node.has_value, self.__sub))
+        ):
+            self.__set_value(
+                self.__op(*map(lambda n : n.get(), self.__sub)),
+                False
+            )
+            self.__notify()
+
+    def __notify(self):
+        dump_map(Node.__try_update, self.__con)
+
+    # def reset(self):
+    #     if self and not self.__sticky :
+    #         self.__value = None
+    #         self.__has_value = False
+    #         dump_map(Node.reset, self.__con)
 
     def __bool__(self):
         return self.has_value()
 
 
+
 class basic_bench:
     def __init__(self):
-        self.raw = Node()
-        self.mean = Node(statistics.mean, self.raw)
-        self.median = Node(statistics.median, self.raw)
-        self.min = Node(min, self.raw)
-        self.max = Node(max, self.raw)
-        self.jitter = Node(lambda a, b : a - b, self.max, self.min)
+        self._raw = Node()
+        self._mean = Node(statistics.mean, self._raw)
+        self._median = Node(statistics.median, self._raw)
+        self._min = Node(min, self._raw)
+        self._max = Node(max, self._raw)
+        self._jitter = Node(operator.sub, self._max, self._min)
+        self._stdev = Node(statistics.stdev, self._raw)
 
-# class bench_micro_bench:
-#     def __init__(self):
-#         self.reset()
 
-#     def reset(self):
-#         self.raw = None
-#         self.mean = None
-#         self.median = None
-#         self.min = None
-#         self.max = None
-#         self.jitter = None
-#         self.stddev = None
 
-#     def set_jitter(self, jitter):
-#         self.jitter = jitter
+def addprop(clazz, name, fget=None, fset=None, fdel=None, doc=None):
+    setattr(clazz, name, property(fget, fset, fdel, doc))
+
+def get_get_f(name):
+    return lambda obj, n=name: getattr(obj, n).get()
+
+def get_set_f(name):
+    return lambda obj, value, n=name: getattr(obj, n).set(value)
+
+def get_del_f(name):
+    return lambda obj, n=name: None #del(getattr(obj, n))
+
+def privatename(name):
+    return '_' + name
+
+def gen_prop_f(name, doc=None):
+    pname = privatename(name)
+    return (name, get_get_f(pname), get_set_f(pname), get_del_f(pname))
+
+def addproplistname(clazz, list_name):
+    dump_map(lambda name : partial(addprop, clazz)(*gen_prop_f(name)), list_name)
+
+addproplistname(basic_bench, ["raw", "mean", "median", "min", "max", "jitter", "stdev"])
+
+
+
+class google_bench:
+    def mean(bench):
+        return bench["real_time"]
+
+    def name_match(bench, name):
+        return bench["name"] == name
+
+    def bench_by_name(benchs, name):
+        return next((bench for bench in benchs if google_bench.name_match(bench, name)), None)
+
+    def parse_bench(bench):
+        bb = basic_bench()
+        bb.mean = google_bench.mean(bench)
+
+        return bb
+
+    def benchs(jdata):
+        return jdata["benchmarks"]
+
+    def parse_names_bench(jdata, name):
+        return google_bench.parse_bench(google_bench.bench_by_name(google_bench.benchs(jdata), name))
+
+
+bench_map = {'granite' : google_bench, 'google' : google_bench}
+
+
+def load_benchs(jdata):
+    res = {}
+    for prop in jdata["properties"]:
+        res[prop["test"]] = [bench for bench in prop["bench"]]
+    return res
+
+
+class bench_micro_bench:
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.raw = None
+        self.mean = None
+        self.median = None
+        self.min = None
+        self.max = None
+        self.jitter = None
+        self.stddev = None
+
+    def set_jitter(self, jitter):
+        self.jitter = jitter
     
 
 
 
-# def parse_stat_file_google_json(file):
-#     jdata = json.load(file)
+def parse_stat_file_google_json(file):
+    jdata = json.load(file)
 
-#     mean = 
+    mean = 
 
 
-# def load_stat_file_google_json(filename):
-#     with open(filename) as f :
-#         jdata = json.loads(f.read())
+def load_stat_file_google_json(filename):
+    with open(filename) as f :
+        jdata = json.loads(f.read())
     
-#     return 
+    return 
 
-# def load_stat_file(filename, generator):
-#     pass
+def load_stat_file(filename, generator):
+    pass
 
 # class state:
 #     def __init__(data):
