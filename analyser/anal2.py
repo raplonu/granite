3bd05b7873e00ef3a4c52f25e526143d16cf91cb
Julien Bernard <julien.bernard@external.softbankrobotics.com>
2019-05-27 18:47:09 +0200

    progress

--- analyser/anal2.py
+++ analyser/anal2.py
@@ -9,3 +9,20 @@ dump_map = lambda func, *args : (lambda *args : None)(*map(func, *args))
 
 def constant_function(value):
     return lambda : value
+
+
+def partial_callable(fn, *cargs):
+    return partial(fn, *map(lambda c: c(), cargs))
+
+def map_callable(fn, *cargs):
+    return map()
+
+class lazy_get:
+    def __init__(self, data, key):
+        self.call = constant_function(data[key]) if has_key(data, key) else None
+            
+    def or_compute(slef, f, *args):
+        if self.call is None:
+            self.call = partial_callable(f, *args)
+
+        return self
