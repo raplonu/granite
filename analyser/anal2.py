200612b428b496dd14ce8482e4d663b44594ee01
Julien Bernard <julien.bernard@external.softbankrobotics.com>
2019-05-28 16:15:33 +0200

    add some anal

--- analyser/anal2.py
+++ analyser/anal2.py
@@ -5,6 +5,7 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 import operator
 import  boltons.cacheutils as cu
+import funcy
 
 dump_map = lambda func, *args : (lambda *args : None)(*map(func, *args))
 
@@ -16,6 +17,7 @@ def compose_deco(*decos):
         for deco in reversed(decos):
             fn = deco(fn)
         return fn
+        # return funcy.compose(fn, *reversed(decos))
     return composed
 
 decor_and_wraps = lambda fn, *decos: compose_deco(ft.wraps(fn), *decos)
@@ -40,7 +42,7 @@ lazy_cached = lambda cache : compose_deco(lazy, cached(cache))
 json_data_cache = cu.LRU(max_size=128)
 
 @decor_and_wraps(json.load, lazy_cached(json_data_cache))
-def jdata_load(filename):
+def json_load(filename):
     print("Loading JSON file :{}".format(filename))
     return json.load(open(filename))
 
@@ -52,6 +54,46 @@ def prop_access(jdata_loader, property_acc):
     res = property_acc(jdata_loader())
     print("Getting result : {}".format(res))
     return res
+
+
+def getter(key):
+    return lambda data: data[key]
+
+def is_value(value):
+    return lambda data: value == data
+
+def first(predicate):
+    return lambda data: next((d for d in data if predicate(d)), None)
+
+def flatter(key):
+    return lambda data: [d[key] for d in data]
+
+def access_to(*getters):
+    def wrapper_access_to(data):
+        for getter in getters:
+            data = getter(data)
+        return data
+    return wrapper_access_to
+
+jdata = json_load("data/fun1_reg.json")
+
+def match_field(key, value):
+    return funcy.compose(is_value(value), getter(key))
+
+fun1_mean = prop_access(jdata,
+    access_to(
+        getter("benchmarks"),
+        first(match_field("name", "BM_Fun1")),
+        getter("real_time")))
+
+
+
+fun1_rep = prop_access(jdata,
+    access_to(
+        getter("benchmarks"),
+        first(match_field("name", "BM_Fun1")),
+        getter("repetitions")))
+
 
 
 
