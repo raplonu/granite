89d31489ca006c1ce576b66fc888b991eeb80c76
raplonu <raplonu.jb@gmail.com>
2019-05-29 09:51:06 +0200

    add csv

--- analyser/anal2.py
+++ analyser/anal2.py
@@ -1,99 +1,109 @@
-import json
-import statistics
 import functools as ft
-import numpy as np
-from sklearn.linear_model import LinearRegression
-import operator
+import funcy as fc
+
 import  boltons.cacheutils as cu
-import funcy
+
+import json
+import csv
+
+import numpy as np
+import statistics
+from sklearn.linear_model import LinearRegression
+
+import operator
 
 dump_map = lambda func, *args : (lambda *args : None)(*map(func, *args))
 
-def constant_function(value):
-    return lambda : value
+@fc.decorator
+def partialize(fn):
+    return fc.partial(fn)
 
-def compose_deco(*decos):
-    def composed(fn):
-        for deco in reversed(decos):
-            fn = deco(fn)
-        return fn
-        # return funcy.compose(fn, *reversed(decos))
-    return composed
-
-decor_and_wraps = lambda fn, *decos: compose_deco(ft.wraps(fn), *decos)
-
-def lazy(fn):
-    @ft.wraps(fn)
-    def wrapper_lazy(*args, **kwargs):
-        return ft.partial(fn, *args, **kwargs)
-    return wrapper_lazy
+decor_and_wraps = lambda fn, *decos: fc.compose(ft.wraps(fn), *decos)
 
 @ft.wraps(cu.cached)
-def cached(cache):
-    def inter_cached(fn):
-        @decor_and_wraps(fn, cu.cached(cache))
-        def wrapper_cached(*args, **kwargs):
-            return fn(*args, **kwargs)
-        return wrapper_cached
-    return inter_cached
+def cached(cache, scoped=True, typed=False, key=None):
+    return lambda fn : decor_and_wraps(fn,
+        cu.cached(cache, scoped, typed, key))(fn) 
 
-lazy_cached = lambda cache : compose_deco(lazy, cached(cache))
 
-json_data_cache = cu.LRU(max_size=128)
+def partialize_cached(cache, scoped=True, typed=False, key=None):
+    return fc.compose(partialize, cached(cache, scoped, typed, key))
 
-@decor_and_wraps(json.load, lazy_cached(json_data_cache))
+file_data_cache = cu.LRU(max_size=128)
+
+@decor_and_wraps(json.load, partialize, cached(file_data_cache))
 def json_load(filename):
     print("Loading JSON file :{}".format(filename))
     return json.load(open(filename))
 
+@partialize_cached(file_data_cache)
+def csv_load(filename):
+    print("Loading CSV file :{}".format(filename))
+    return list(csv.reader(open(filename)))
 
 prop_cache = cu.LRU(max_size=2048)
 
-@lazy_cached(prop_cache)
+@partialize_cached(prop_cache)
 def prop_access(jdata_loader, property_acc):
     res = property_acc(jdata_loader())
     print("Getting result : {}".format(res))
     return res
 
+@partialize
+def get(key, seq):
+    return seq[key]
 
-def getter(key):
-    return lambda data: data[key]
+@partialize
+def equal_to(arg1, arg2):
+    return arg1 == arg2
 
-def is_value(value):
-    return lambda data: value == data
 
-def first(predicate):
-    return lambda data: next((d for d in data if predicate(d)), None)
+first_filter = partialize(fc.compose(fc.first, fc.filter))
 
-def flatter(key):
-    return lambda data: [d[key] for d in data]
-
-def access_to(*getters):
-    def wrapper_access_to(data):
-        for getter in getters:
-            data = getter(data)
-        return data
-    return wrapper_access_to
+@partialize
+def select_values(key, seq):
+    return [s[key] for s in seq]
 
 jdata = json_load("data/fun1_reg.json")
+cdata = csv_load("data/fun1_runtime.csv")
 
-def match_field(key, value):
-    return funcy.compose(is_value(value), getter(key))
-
-fun1_mean = prop_access(jdata,
-    access_to(
-        getter("benchmarks"),
-        first(match_field("name", "BM_Fun1")),
-        getter("real_time")))
+def field_match(key, value):
+    return fc.compose(equal_to(value), get(key))
 
 
+get_bench = get("benchmarks")
 
-fun1_rep = prop_access(jdata,
-    access_to(
-        getter("benchmarks"),
-        first(match_field("name", "BM_Fun1")),
-        getter("repetitions")))
+find_bench_by_name = lambda name : fc.rcompose(get_bench, first_filter(field_match("name", name)))
 
+fun1_bench = prop_access(jdata, find_bench_by_name("BM_Fun1")) 
+
+fun1_mean = prop_access(fun1_bench, get("real_time"))
+
+fun1_rep = prop_access(fun1_bench, get("repetitions"))
+
+
+def match_at(value, pos):
+    return fc.compose(equal_to(value), get(pos))
+
+# def find_pos(value):
+#     return 
+
+fun1_run = prop_access(cdata,
+    fc.rcompose(
+        first_filter(match_at("BM_Fun1", 0)),
+        get(2)))
+
+
+
+
+
+
+# def partialize(*p_args, **p_kwargs):
+#     def inter_partialize(fn):
+#         def wrapper_partialize(*args, **kwargs):
+#             return fn(*p_args, *args, **kwargs, **p_kwargs)
+#         return wrapper_partialize
+#     return inter_partialize
 
 
 
@@ -106,7 +116,7 @@ fun1_rep = prop_access(jdata,
 
 # class lazy_get:
 #     def __init__(self, data, key):
-#         self.call = constant_function(data[key]) if has_key(data, key) else None
+        # self.call = constant_function(data[key]) if has_key(data, key) else None
             
 #     def or_compute(slef, f, *args):
 #         if self.call is None:
